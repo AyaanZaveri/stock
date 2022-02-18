6a4c8b988941b83c6bc862c04d2a45a3588a0bf2
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-02-18 12:16:32 -0500

    Search is working with autocomplete

--- components/Search.tsx
+++ components/Search.tsx
@@ -2,13 +2,27 @@ import React from 'react'
 import axios from 'axios'
 
 interface SearchProps {
-    searchTerm: string
-    setSearchTerm: (searchTerm: string) => void
+  searchTerm: string
+  setSearchTerm: (searchTerm: string) => void
 }
 
-const Search = ({ searchTerm, setSearchTerm } : SearchProps) => {
+const Search = ({ searchTerm, setSearchTerm }: SearchProps) => {
+  const autoComplete = () => {
+    axios
+      .get(
+        `https://cors-anywhere-production-f183.up.railway.app/https://query1.finance.yahoo.com/v7/finance/autocomplete?region=US&lang=en&query=${searchTerm}`
+      )
+      .then((res) => {
+        console.log(res.data)
+      })
+  }
 
-  return <div>Search</div>
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value)
+    autoComplete()
+  }
+
+  return <div><input type="text" value={searchTerm} onChange={handleChange} /></div>
 }
 
 export default Search
