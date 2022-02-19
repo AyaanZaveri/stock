c3c6746f9cb010ddec1514c360581258c25ca703
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-02-18 22:07:56 -0500

    Cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import Search from '../components/Search'
 import axios from 'axios'
+import Trending from '../components/Trending'
 
 const Home = () => {
   return (
     <div>
       <div className="grid place-items-center">
         <Search />
+        <Trending />
       </div>
     </div>
   )
