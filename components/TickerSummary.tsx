49e933063b6fb48a8172011e7ce7fde515ba2399
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-02-19 14:55:30 -0500

    Table

--- components/TickerSummary.tsx
+++ components/TickerSummary.tsx
@@ -24,28 +24,62 @@ const TickerSummary = (priceData: any) => {
   priceData = priceData.priceData
   return (
     <div>
-      <div className="border-b p-3">
-        Previous Close: {priceData.regularMarketPreviousClose}
+      <div className="mt-3 rounded-lg border text-slate-700">
+        <div className="border-b p-3">
+          Previous Close:{' '}
+          <span className="font-bold">
+            {priceData.regularMarketPreviousClose}
+          </span>
+        </div>
+        <div className="border-b p-3">
+          Open: <span className="font-bold">{priceData.regularMarketOpen}</span>
+        </div>
+        <div className="border-b p-3">
+          Bid: <span className="font-bold">{priceData.bid}</span>
+        </div>
+        <div className="border-b p-3">
+          Ask: <span className="font-bold">{priceData.ask}</span>
+        </div>
+        <div className="border-b p-3">
+          Day Range:{' '}
+          <span className="font-bold">{priceData.regularMarketDayRange}</span>
+        </div>
+        <div className="border-b p-3">
+          52 Week Range:{' '}
+          <span className="font-bold">{priceData.fiftyTwoWeekRange}</span>
+        </div>
+        <div className="border-b p-3">
+          Volume:{' '}
+          <span className="font-bold">{priceData.regularMarketVolume}</span>
+        </div>
+        <div className="border-b p-3">
+          Market Cap: <span className="font-bold">{priceData.marketCap}</span>
+        </div>
+        <div className="border-b p-3">
+          Time: <span className="font-bold">{priceData.regularMarketTime}</span>
+        </div>
+        <div className="border-b p-3">
+          Currency: <span className="font-bold">{priceData.currency}</span>
+        </div>
+        <div className="border-b p-3">
+          EPS (TTM):{' '}
+          <span className="font-bold">{priceData.epsTrailingTwelveMonths}</span>
+        </div>
+        <div className="border-b p-3">
+          Market State:{' '}
+          {
+            (priceData.marketState = 'CLOSED' ? (
+              <span className="font-bold text-red-500">
+                {priceData.marketState}
+              </span>
+            ) : (
+              <span className="font-bold text-emerald-500">
+                {priceData.marketState}
+              </span>
+            ))
+          }
+        </div>
       </div>
-      <div className="border-b p-3">Open: {priceData.regularMarketOpen}</div>
-      <div className="border-b p-3">Bid: {priceData.bid}</div>
-      <div className="border-b p-3">Ask: {priceData.ask}</div>
-      <div className="border-b p-3">
-        Day Range: {priceData.regularMarketDayRange}
-      </div>
-      <div className="border-b p-3">
-        52 Week Range: {priceData.fiftyTwoWeekRange}
-      </div>
-      <div className="border-b p-3">
-        Volume: {priceData.regularMarketVolume}
-      </div>
-      <div className="border-b p-3">Market Cap: {priceData.marketCap}</div>
-      <div className="border-b p-3">Time: {priceData.regularMarketTime}</div>
-      <div className="border-b p-3">Currency: {priceData.currency}</div>
-      <div className="border-b p-3">
-        EPS (TTM): {priceData.epsTrailingTwelveMonths}
-      </div>
-      <div className="border-b p-3">Market State: {priceData.marketState}</div>
     </div>
   )
 }
