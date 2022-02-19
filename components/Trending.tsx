c18cbb69c247a272cec7f677d44e776b6953c4fc
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-02-19 13:21:41 -0500

    Ttrending

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -17,27 +17,25 @@ const Trending = () => {
   }
 
   const priceData = async (ticker: string) => {
-    const response = await axios.get(`https://query2.finance.yahoo.com/v6/finance/quote?region=US&lang=en&symbols=${ticker}`)
-    return response.data
+    axios.get(
+      `https://query2.finance.yahoo.com/v6/finance/quote?region=US&lang=en&symbols=${ticker}`
+    ).then(response => {
+      return response.data
+    })
   }
 
   useEffect(() => {
     trendingData()
   }, [])
 
-  useEffect(() => {
-    (async () => {
-      const response = await priceData('AAPL')
-      console.log(response)
-    })();
-  }, [])
+  priceData('AAPL').then((res) => console.log(res))
 
   console.log(trending)
 
   return (
-    <div className="mt-20">
+    <div className="mt-20 flex flex-col items-center justify-center w-screen">
       <div className="mt-5">
-        <div className="flex flex-row overflow-auto whitespace-nowrap">
+        <div className="flex flex-row overflow-auto whitespace-nowrap border w-9/12">
           {trending
             ? trending.map((data: any) => (
                 <div className="border-l p-5 text-slate-700 transition-all hover:cursor-pointer hover:bg-slate-50 hover:underline">
