1266e98ca271441ac92082b2b7a95279b4f3e97f
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-02-18 22:41:09 -0500

    Middleware changes

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,5 +1,5 @@
 import { NextResponse, NextRequest } from 'next/server'
-export async function middleware(req, ev) {
+export async function middleware(req:any) {
     const { pathname } = req.nextUrl
     if (pathname == '/ticker') {
         return NextResponse.redirect(new URL('/', req.url))
