9283c909eb23a06cf1e67be940747507ea9607ec
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-02-18 21:14:37 -0500

    priceData

--- pages/ticker/[id]/index.tsx
+++ pages/ticker/[id]/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import axios from 'axios'
 
-const TickerIndex = () => {
-  return (
-    <div>TickerIndex</div>
+const TickerIndex = ({ priceData }: { priceData: any }) => {
+  console.log(priceData)
+
+  return <div>{JSON.stringify(priceData)}</div>
+}
+
+export const getServerSideProps = async (context: any) => {
+  const { data: priceData } = await axios.get(
+    `https://query2.finance.yahoo.com/v6/finance/quote?region=US&lang=en&symbols=${context.params.id}`
   )
+
+  return {
+    props: {
+      priceData,
+    },
+  }
 }
 
 export default TickerIndex
